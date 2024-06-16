96556261763280a34e5914ed70ad88ece5a15d47
Victor Bulhões <victor.bulhoes@timpelmedical.com>
2024-06-16 19:55:53 -0300

    feat: update user

--- app.js
+++ app.js
@@ -161,6 +161,30 @@ app.delete('/users/:id', checkToken, async(req, res) => {
     }
 });
 
+app.put('/users/update/:id', checkToken, async(req, res) => {
+    const id = req.params.id;
+
+    const user = await User.findById(id);
+    
+    const salt = await bcrypt.genSalt(12);
+
+    const update = {
+        name: req.body.name,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, salt)
+    };
+
+    const conditions = { _id: id };
+
+    try {
+        await User.findOneAndUpdate(conditions, update);
+
+        res.status(200).json({ msg: "Os dados do usuário foram atualizados" });
+    } catch (err) {
+        return res.status(500).json({ msg: "Não foi possível editar os dados do usuário." });
+    }
+});
+
 // Credencials
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
